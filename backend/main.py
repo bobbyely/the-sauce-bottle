e60d724621b52713d6c84e456e11a086bd104455
bobbyely <robert.w.ely@gmail.com>
2025-06-05 23:00:40 +1000

    added linting, pixi

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to The Sauce Bottle!"}
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}!"}
+
